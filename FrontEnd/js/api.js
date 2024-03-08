56ac35a0aa8b1853b8e6edcdc8af8f5eb5a53c88
Brice123456zz <Your Emailbrice.seguret@gmail.com>
2024-03-08 16:52:29 +0100

    gitignore mj et recepetion des categories et works + page login

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -4,24 +4,44 @@ const categories = await getCategories();
 }*/
 
 /* recuperation des Categorie */ 
-
 const filtres = document.querySelector(".filtres");
+const galleryContainer = document.querySelector(".gallery");
 
-fetch('http://localhost:5678/api/categories')
-  .then(response => response.json())
-  .then(html => { console.log(html); 
+filtreData();
+galleryData();
+
+
+async function filtreData() {
+  try {
+    const response = await fetch('http://localhost:5678/api/categories');
+    const html = await response.json();
+    console.log(html); 
     for (const category of html) {
       const btnFiltres = document.createElement("button");
       btnFiltres.innerText = category.name;
       filtres.appendChild(btnFiltres);
     }
-  });
-
-
+  } catch (error) {
+    console.error('Une erreur s\'est produite :', error);
+  }
+}
 // {accept: "application/json", 'Content-Type': "application/json" } 
 
-
-/*const res = await fetch*/
+async function galleryData() {
+ 
+    const res = await fetch('http://localhost:5678/api/works');
+    const data = await res.json();
+    for (const item of data) {
+      const figureGallery = document.createElement("figure");
+      const imgGallery = document.createElement("img");
+      const figcapGallery = document.createElement("figcaption");
+      imgGallery.src = item.imageUrl;
+      figcapGallery.innerText = item.title;
+      galleryContainer.appendChild(figureGallery);
+      figureGallery.appendChild(imgGallery);
+      figureGallery.appendChild(figcapGallery);
+    }
+}
 
 /* 
 -H 'accept: application/json' \
